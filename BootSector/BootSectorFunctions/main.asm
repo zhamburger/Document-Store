a099ab7879b648c311a75985c7d73f0a412323f5
Zachary Hamburger <Hamburger.Zachary@gmail.com>
2021-07-06 14:55:40 -0400

    program that uses functions to print Hello World

---
+++ BootSector/BootSectorFunctions/main.asm
@@ -0,0 +1,35 @@
+[org 0x7c00]			; set offset to boot sector code
+
+;=============================MAIN===============================
+
+; 'call' saves the address of execution
+; so that the function can return to the control flow in main
+
+
+mov bx, HELLO_WORLD
+call print			; print(HELLO_WORLD)
+
+call print_new_line		; print('\n')
+
+mov bx, GOODBYE_WORLD
+call print			; print(GOODBYE_WORLD)
+
+call print_new_line
+
+jmp $
+
+
+;=============================Imports============================
+
+%include "print.asm"		; import print file
+
+;=============================Strings============================
+HELLO_WORLD:
+	db 'Hello, World', 0	
+
+GOODBYE_WORLD:
+	db 'Goodbye, World', 0
+
+;============================Magic Number========================
+times 510-($-$$) db 0
+dw 0xaa55
